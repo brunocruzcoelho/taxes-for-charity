dd3157c48ee2a07010828fbe6281121d96acdc23
bruno coelho <bruno.coelho@gmail.com>
2015-02-18 22:47:09 +0000

    makes admin import action accessible only if you have a key

--- app/controllers/admin/associations_controller.rb
+++ app/controllers/admin/associations_controller.rb
@@ -1,10 +1,13 @@
 class Admin::AssociationsController < ApplicationController
   require 'csv'
+  before_action :validate_admin_key
 
   def index
   end
 
   def import
+    Association.delete_all if params[:delete_asociations_before_import]
+
     if file = params[:file]
       CSV.foreach(file.path, headers: true) do |row|
         Association.create(nif: row[0], name: row[1], city: row[2])
@@ -13,6 +16,17 @@ class Admin::AssociationsController < ApplicationController
       redirect_to admin_url, notice: "Associations CSV imported."
     else
       redirect_to admin_url, notice: "Please choose a file."
+    end
+  end
+
+  private
+
+  def validate_admin_key
+    session[:admin_key] ||= params[:admin_key]
+
+    unless Rails.application.config.admin_key.eql?(session[:admin_key])
+      session[:admin_key] = nil
+      flash[:error] = "You must have an admin key to perform admin actions"
     end
   end
 end
