b6a6527f7b7ba0ae29c6b4feb9ad652c9ec7aa12
bruno coelho <bruno.coelho@gmail.com>
2015-02-27 21:09:27 +0000

    implements search of associations by name or city

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -2,4 +2,13 @@ class AssociationsController < ApplicationController
   def index
     @associations = Association.all
   end
+
+  def search
+    redirect_to action: :index and return if params[:search].blank?
+
+    search_field = params[:search_field].blank? ? 'name' : params[:search_field]
+
+    @associations = Association.where("lower(#{search_field}) LIKE ?", "%#{(params[:search]).downcase}%")
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'associations#index'
 
-  resources :associations, only: :index
+  resources :associations, only: [:index] do
+    collection do
+      get :search
+    end
+  end
 
   namespace :admin do
     get '/', to: 'associations#index'
