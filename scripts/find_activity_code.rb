3ca6b1287cd267903818b40f441f29f9a1ad1246
bruno coelho <bruno.coelho@gmail.com>
2015-03-01 23:02:32 +0000

    changes script to create an Activity Code if doesn't exist one improves output

--- scripts/find_activity_code.rb
+++ scripts/find_activity_code.rb
@@ -1,11 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-VERBOSE = false
+VERBOSE = true
 
 BASE_URL = "http://www.einforma.pt/servlet/app/portal/ENTP/prod/ETIQUETA_EMPRESA_CONTRIBUINTE/nif/"
 
-CharityAssociation.includes(:activity_code).where( activity_codes: { id: nil }).find_in_batches(batch_size: 50) do |batch|
+CharityAssociation.includes(:activity_code).where( activity_codes: { id: nil }).find_in_batches(batch_size: 100) do |batch|
   # puts 'Sleeping for 1 min ...' if VERBOSE
   # sleep(60)
 
@@ -18,17 +18,23 @@ CharityAssociation.includes(:activity_code).where( activity_codes: { id: nil }).
 
     doc.css('span.category').each do |item|
       activity_code_name = item.inner_html
-      puts "got this: #{activity_code_name} \n" if VERBOSE
+      puts "\t\t got this: #{activity_code_name} \n" if VERBOSE
 
       # the results are sorted for the one with bigger id because it's the more specific and maybe the correct!
       activity_code = ActivityCode.where("lower(name) LIKE ?", "%#{activity_code_name.downcase}%").order(code: :desc).first
 
       if activity_code
-        puts "found this: #{activity_code.name}" if VERBOSE
-
-        charity_association.activity_code = activity_code
-        puts "#{charity_association.name} updated!" if charity_association.save
+        puts "\t\t FOUND activity code: #{activity_code.name}" if VERBOSE
+      else
+        activity_code = ActivityCode.new(name: activity_code_name)
+        activity_code.save
+        puts "\t\t created activity code: #{activity_code.name}" if VERBOSE
       end
+
+      charity_association.activity_code = activity_code
+      charity_association.save
+
+      puts "\n\n" if VERBOSE
     end
   end
 end
