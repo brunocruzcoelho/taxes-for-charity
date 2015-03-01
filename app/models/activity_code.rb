a8cce45f6168a01f4513113d05d16a964a545a68
bruno coelho <bruno.coelho@gmail.com>
2015-03-01 23:04:01 +0000

    makes code optional in activity code model

--- app/models/activity_code.rb
+++ app/models/activity_code.rb
@@ -1,8 +1,8 @@
 class ActivityCode < ActiveRecord::Base
   has_one :charity_association
 
-  validates :name, :code, presence: true
+  validates :name, presence: true
 
   validates :code, numericality: { only_integer: true }
-  validates :code, uniqueness: true
+  validates :code, :name, uniqueness: true
 end
