2a193d8b233f6bae7b339c7957f10cd70c1e179f
Mário  Nzualo <mario.nzualo@gmail.com>
2015-03-02 21:11:15 +0000

    Adds 'active-admin'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'charity_associations#index'
 
   resources :charity_associations, only: [:index] do
