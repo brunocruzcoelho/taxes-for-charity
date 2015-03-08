58685b362983ee06beb698c0fc7621c0930f85d1
bruno coelho <bruno.coelho@gmail.com>
2015-03-08 22:34:35 +0000

    defines search UI and creates a home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'charity_associations#index'
+
+  root 'home#index'
 
   resources :charity_associations, only: [:index] do
     collection do
