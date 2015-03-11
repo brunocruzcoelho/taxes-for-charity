b79a2db5617a261db674eaaf09532015ae0b8214
André Luís <me@andr3.net>
2015-03-11 14:48:18 +0000

    Add teaser page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'home#index'
+  root 'home#teaser'
+  get '/app' => 'home#index'
 
   resources :charity_associations, only: [:index] do
     collection do
