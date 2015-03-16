f28f58efb1d961b2bdcc95b587f6d54ae6875dd3
André Luís <me@andr3.net>
2015-03-16 18:55:50 +0000

    Disable teaser and replace / with the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'home#teaser'
-  get '/app' => 'home#index'
+  root 'home#index'
 
   resources :charity_associations, only: [:index] do
     collection do
