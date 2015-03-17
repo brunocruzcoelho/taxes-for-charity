3f3ecf72964924669b108229e6c0bee5e0f4884e
bruno coelho <bruno.coelho@gmail.com>
2015-03-17 23:51:08 +0000

    implements search using ajax

--- app/controllers/charity_associations_controller.rb
+++ app/controllers/charity_associations_controller.rb
@@ -1,12 +1,18 @@
 class CharityAssociationsController < ApplicationController
   def search
-    redirect_to root_url, notice: 'Introduza termos de pesquisa, por favor.' and return if params[:search_term].blank?
+    render(partial: 'search_results', locals: { message: 'Introduza termos de pesquisa, por favor.' }) and return if params[:search_term].blank?
 
     chain = default_chain(params[:search_term])
     chain = chain.where(city: params[:city]) unless params[:city].blank?
     chain = chain.where(activity_code_id: params[:activity_code]) unless params[:activity_code].blank?
 
     @charity_associations = chain
+
+    if @charity_associations.any?
+      render partial: 'search_results'
+    else
+      render partial: 'search_results', locals: { message: 'Não foram encontradas instituições. Tente redefinir os termos de pesquisa, por favor.' }
+    end
   end
 
   private
